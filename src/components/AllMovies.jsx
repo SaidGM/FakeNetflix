ec89a7eaca91d8ad5a2a9b0300561aea79f37ae6
javier <villarestrepocarlos@gmail.com>
2023-02-14 10:19:17 +0100

    version0.3

--- src/components/AllMovies.jsx
+++ src/components/AllMovies.jsx
@@ -1,12 +1,17 @@
+// import { RepeatOutlined } from '@material-ui/icons';
 import React from 'react'
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
 
 function AllMovies() {
+
+
   return (
     <section className='section'>
       <h4 className='h4'>All movies</h4>
       <div className='carrousel'>
         <article className='article'>
-
+          <AiOutlineHeart className= "heart"/>
         </article>
         <article className='article'>
           
